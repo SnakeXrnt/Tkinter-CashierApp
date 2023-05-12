62ddbeb76cf00ec4a8e674327ef532cc968e4bee
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2023-05-12 21:17:50 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox as msg
+from time import strftime
+import os
+import sys
+import tkinter.ttk as ttk
+#from ttkthemes import ThemedStyle
+from tkinter import Tk, Label, StringVar, ttk, Entry, Button, LabelFrame, Menu, Frame
+
+from json import *
+from datetime import *
+from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
